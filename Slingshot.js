0d9dde5cee186e4f6b434840f0328d9a592d562a
Preeshika Dattani <73643686+preeshika-whitehat@users.noreply.github.com>
2021-02-25 19:09:57 +0800

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,36 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
+
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+
+    image(this.sling1,210,20);
+    image(this.sling2,180,20);
+
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+           line(pointA.x, pointA.y, pointB.x-10, pointB.y-50);
+           line(pointA.x, pointA.y, pointB.x+35, pointB.y-50);
+        }
+    }
+    
+}
